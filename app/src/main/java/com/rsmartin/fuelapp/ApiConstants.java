0e438f108c0c735b7d352ed9db72cd1d13eb43d5
RafaelSMartin <rafaels.martin.dev@gmail.com>
2020-09-28 19:34:28 +0200

    Limpieza de codigo y solo main en kotlin

--- app/src/main/java/com/rsmartin/fuelapp/ApiConstants.java
+++
@@ -1,6 +0,0 @@
-package com.rsmartin.fuelapp;
-
-public class ApiConstants {
-
-    public static final String BASE_URL = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/";
-}
---
+++ app/src/main/java/com/rsmartin/fuelapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rsmartin.fuelapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+    }
+}
